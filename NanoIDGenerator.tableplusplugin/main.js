a8e2a9c88ab78e03621c96c57a7ff8341c51e8d7
Vasanth Srivatsa <vsnthdev@gmail.com>
2025-05-31 11:21:11 +0530

    💀 remove UUID functionality

--- NanoIDGenerator.tableplusplugin/main.js
+++ NanoIDGenerator.tableplusplugin/main.js
@@ -1,19 +1,18 @@
 import { customAlphabet } from 'nanoid/non-secure'
-import { getUUID } from './library/helper'
 
-var setContent = function (context, uuid) {
+var setContent = function (context, nanoid) {
     // Get all the items
     var row = context.clickedRow()
     var col = context.clickedColumn()
     var item = context.currentItem()
 
     if (row == null || col == null || item == null) {
-        context.alert('Error', 'No item cliked')
+        context.alert('Error', 'No item clicked')
         return
     }
 
-    if (uuid == null) {
-        context.alert('Error', 'Could not generate UUID')
+    if (nanoid == null) {
+        context.alert('Error', 'Could not generate Nanoid')
         return
     }
 
@@ -21,37 +20,13 @@ var setContent = function (context, uuid) {
     row.setConstant(col.name, '')
 
     // Update row value
-    row.update(col.name, uuid)
+    row.update(col.name, nanoid)
 
     // Add to update queue
     item.addUpdate(row)
 
     // Notify the change
     context.update()
-}
-
-var v1 = function (context) {
-    try {
-        let uuid = getUUID("1", true)
-        setContent(context, uuid)
-    } catch (err) {
-        context.alert('Error', err)
-    }
-}
-
-var v4 = function (context) {
-    let uuid = getUUID("4", true)
-    setContent(context, uuid)
-}
-
-var v1nohyphens = function (context) {
-    let uuid = getUUID("1", false)
-    setContent(context, uuid)
-}
-
-var v4nohyphens = function (context) {
-    let uuid = getUUID("4", false)
-    setContent(context, uuid)
 }
 
 var nanoid = function (context) {
@@ -64,8 +39,4 @@ var nanoid = function (context) {
     setContent(context, nanoid)
 }
 
-global.v1 = v1
-global.v4 = v4
-global.v1nohyphens = v1nohyphens
-global.v4nohyphens = v4nohyphens
 global.nanoid = nanoid
